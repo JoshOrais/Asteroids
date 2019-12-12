051ce636c31e4ce5d4e698e582024bced3aaac13
JoshOrais <michaeljoshuaorais@gmail.com>
2019-12-12 23:56:57 +0800

    hunter missile commented out

--- src/game/asteroids/entities/HunterSpawner.java
+++ src/game/asteroids/entities/HunterSpawner.java
@@ -7,33 +7,6 @@
 
 // import org.joml.*;
 
-<<<<<<< HEAD
-public class HunterSpawner extends Particle{
-
-  private int growth_direction = -1;
-
-  public HunterSpawner(Vector3f loc){
-    this.position = loc;
-
-    this.scale = 30.2f;
-    this.hitbox = new HitBox(new Vector2f(loc.x, loc.y), scale);
-    setBounded(false);
-    AnimatedSprite asprite = new AnimatedSprite(2, lifespan);
-    asprite.addFrame(ResourceLoader.getTexture("puff_1"));
-    asprite.addFrame(ResourceLoader.getTexture("puff_2"));
-    setSprite(asprite);
-    setLifeSpan(lifespan);
-  }
-
-  public void update(float interval){
-    age(interval);
-    if (scale < 15.f) growth_direction = 1;
-    this.scale += 2.5 * interval * growth_direction;
-    getSprite().update(interval);
-  }
-
-}
-=======
 // public class SplashParticle extends Particle{
 
 //   public SplashParticle(Vector3f loc){
@@ -57,4 +30,3 @@ public class HunterSpawner extends Particle{
 
 
 // }
->>>>>>> d266d17941f002d1dd205f569a22a41d39ea4a02
